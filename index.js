401369797dce18ca8da74097081ffd72eaa26650
User <user@Mac.local>
2018-05-15 17:39:25 +1000

    added index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+  
+var filteredStrA = '';
+for(let char of stringA) {
+  if( char.toLowerCase() != char.toUpperCase()){
+    filteredStrA += char;
+   }
+  }
+var filteredStrB = '';
+for(let char of stringB) {
+  if(char.toLowerCase() != char.toUpperCase()){
+    filteredStrB += char;
+    }
+  }
+  return filteredStrB.split('').sort().join('') == filteredStrA.split('').sort().join('');
+
+}
